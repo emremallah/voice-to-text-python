f11a949c73813c0da6ed4b06d5232a3dde458095
emremallah <skyai.company@gmail.com>
2024-04-29 23:09:57 +0330

    upload files

---
+++ voice_to_text.py
@@ -0,0 +1,14 @@
+import speech_recognition as sr
+
+# ایجاد یک شیء شناسایی
+r = sr.Recognizer()
+
+# استفاده از میکروفون به عنوان منبع ورودی
+with sr.Microphone() as source:
+    print("در حال گوش دادن...")
+    # خواندن داده‌های صوتی از میکروفون پیش فرض
+    audio_data = r.record(source, duration=5)
+    print("در حال شناسایی...")
+    # تبدیل سخن به متن با استفاده از API شناسایی سخن گوگل
+    text = r.recognize_google(audio_data)
+    print(text)
